3b8bd4a5381501f3b333d3589c7eeaa93a94537f
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-04-02 23:41:23 +0530

    Updating the build and run scripts for docker.

--- devops/docker/run-dev.sh
+++ devops/docker/run-dev.sh
@@ -4,4 +4,4 @@ docker run -it --rm -p 3000:3000 \
   -e GO_APP_ENV=$GO_APP_ENV \
   -e PORT=3000 \
   -e DATABASE_URL="postgres://$USER@docker.for.mac.host.internal/$DB_NAME?sslmode=disable" \
-  gauravagarwalr/yaes-server
+  gauravagarwalr/yet-another-expense-splitter
