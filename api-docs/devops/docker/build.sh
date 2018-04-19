85366fac113068d876a4575b3b04492373992fa2
Gaurav Agarwal <gauravagarwalr@gmail.com>
2018-04-19 23:30:46 +0530

    Using alpine. Fixing script. Renaming project-resources.branch.

--- api-docs/devops/docker/build.sh
+++ api-docs/devops/docker/build.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+ACTUAL_WD=$PWD
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker build -f $DIR/../../Dockerfile -t gauravagarwalr/yaes-api-docs:latest .
+cd $DIR/../..
+
+docker build -t gauravagarwalr/yaes-api-docs:latest .
 docker push gauravagarwalr/yaes-api-docs:latest
+
+cd $ACTUAL_WD
